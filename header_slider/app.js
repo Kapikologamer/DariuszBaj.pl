c3ab9cb0ae1742733ca51e123333c14b76d012ef
Kapikologamer17 <kacper.ksi@onet.pl>
2023-08-22 23:34:07 +0200

    init

--- header_slider/app.js
+++ header_slider/app.js
@@ -1,8 +1,11 @@
 function mobile_menu() {
     var mobile_menu = document.getElementById('mobile_menu');
+    var body = document.getElementsByTagName('body')[0];
     if (mobile_menu.style.display === "none") {
         mobile_menu.style.display = "block";
+        body.style.overflow = "hidden";
     } else {
         mobile_menu.style.display = "none";
+        body.style.overflow = "";
     }
 }
