264d4ccbd3bd5cf51c174b3b7cc68db90013723d
Kapikologamer17 <kacper.ksi@onet.pl>
2023-08-26 00:56:00 +0200

    init

--- header_slider/app.js
+++ header_slider/app.js
@@ -9,3 +9,25 @@ function mobile_menu() {
         body.style.overflow = "";
     }
 }
+
+function dropdown_arrow(x) {
+    const ID = 'dropdown_arrow'+ x;
+    var arrow = document.getElementById(ID);
+    if (arrow.classList.contains('bx-chevron-down')) {
+        arrow.classList.remove('bx-chevron-down');
+        arrow.classList.add('bx-chevron-up');
+    } else {
+        arrow.classList.remove('bx-chevron-up');
+        arrow.classList.add('bx-chevron-down');
+    }
+}
+
+function dropdown(x) {
+    const ID = 'dropdown'+ x;
+    var dropdown = document.getElementById(ID);
+    if (dropdown.style.display === "none") {
+        dropdown.style.display = "block";
+    } else {
+        dropdown.style.display = "none";
+    }
+}
