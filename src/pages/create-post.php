4615cf2043451df1a6840bb8da9c9d2be26733c4
Kapikologamer17 <kacper.ksi@onet.pl>
2023-09-01 21:25:06 +0200

    init

---
+++ src/pages/create-post.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pl-PL">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="theme-color" content="#1D1D1F">
+    <link rel="stylesheet" href="../css/create-post.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="shortcut icon" href="../assets/icons/clipboard.svg" type="image/x-icon">
+    <title>Dodaj Post</title>
+</head>
+<body>
+    <header>
+        <h1>Dodaj nowy post</h1>
+    </header>
+    <form method="post">
+        <h4>Tytuł posta</h4>
+        <input type="text" name="title" id="input-title">
+        <h4>Opis posta</h4>
+        <textarea type="text" name="description" id="input-description"></textarea>
+        <button type="submit" id="add_post">Dodaj</button>
+    </form>
+    <form enctype="multipart/form-data" action="upload-file.php" method="post">
+        <h4 class="infil">Dołącz plik</h4>
+        <input type="file" name="file" id="input-file">
+        <label for="input-file">
+            <i class='bx bx-export'></i>
+            Wybierz Plik
+        </label>
+        <button class="infil2" type="submit" id="upload_file">Załącz plik</button>
+    </form>
+</body>
+</html>
